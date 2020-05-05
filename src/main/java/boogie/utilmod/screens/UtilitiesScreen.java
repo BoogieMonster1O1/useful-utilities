172ecaefb581da5ac94b69448d239866fdc53fc9
Shrish Deshpande <shrish.deshpande@icloud.om>
2020-05-05 18:51:44 +0530

    More buttonzz

--- src/main/java/boogie/utilmod/screens/UtilitiesScreen.java
+++ src/main/java/boogie/utilmod/screens/UtilitiesScreen.java
@@ -9,11 +9,8 @@ import io.github.cottonmc.cotton.gui.widget.WSprite;
 import io.github.cottonmc.cotton.gui.widget.data.Alignment;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
-import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
-import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
 public class UtilitiesScreen extends LightweightGuiDescription {
@@ -23,17 +20,27 @@ public class UtilitiesScreen extends LightweightGuiDescription {
         setRootPanel(root);
         root.setSize(356, 240);
 
-        WSprite icon = new WSprite(new Identifier("minecraft:textures/block/furnace_front_on.png"));
-        root.add(icon, 0, 2, 1, 1);
+        WSprite left_icon = new WSprite(new Identifier("minecraft:textures/block/furnace_front_on.png"));
+        root.add(left_icon, 0, 0, 2, 2);
+        WSprite right_icon = new WSprite(new Identifier("minecraft:textures/block/crafting_table_side.png"));
+        root.add(right_icon, 16, 0, 2, 2);
 
         WButton calc = new WButton(new TranslatableText("gui.utilities.calculator"));
-        root.add(calc, 0, 3, 4, 1);
+        calc.setEnabled(false);
+        root.add(calc, 0, 3, 6, 1);
         WButton cps = new WButton(new TranslatableText("gui.utilities.cps"));
-        root.add(cps, 0, 5, 4, 1);
+        cps.setEnabled(false);
+        root.add(cps, 7, 3, 6, 1);
+        WButton portal = new WButton(new TranslatableText("gui.utilities.portal"));
+        portal.setEnabled(false);
+        root.add(portal, 14, 3, 6, 1);
+        WButton random = new WButton(new TranslatableText("gui.utilities.random"));
+        random.setEnabled(false);
+        root.add(random, 0, 5, 6, 1);
 
         WLabel label = new WLabel(new TranslatableText("gui.utilities.title"), 0x000000);
         label.setAlignment(Alignment.CENTER);
-        root.add(label, 2, 2, 2, 1);
+        root.add(label, 7, 1, 6, 1);
 
         root.validate(this);
     }
