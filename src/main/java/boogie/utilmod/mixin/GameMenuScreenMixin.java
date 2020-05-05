126b6e903b1c617c4c3305d75c9ccb2c0528ff98
Shrish Deshpande <shrish.deshpande@icloud.om>
2020-05-05 14:04:46 +0530

    Fixed Button

--- src/main/java/boogie/utilmod/mixin/GameMenuScreenMixin.java
+++ src/main/java/boogie/utilmod/mixin/GameMenuScreenMixin.java
@@ -18,7 +18,7 @@ public class GameMenuScreenMixin extends Screen {
 
     @Inject( method = "initWidgets", at = @At("RETURN"))
     public void lol(CallbackInfo ci){
-        this.addButton(new ButtonWidget(this.width / 2 - 102, this.height / 4 + 144 + -16, 48, 20, I18n.translate("gui.utilities"), (buttonWidgetx) -> {
+        this.addButton(new ButtonWidget(this.width / 2 - 102, this.height / 4 + 144 + -16, 98, 20, I18n.translate("gui.utilities"), (buttonWidgetx) -> {
         }));
     }
 }
