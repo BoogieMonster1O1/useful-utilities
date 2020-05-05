81feb63b595cd4f40fecf3dadf3b018a69d36217
Shrish Deshpande <shrish.deshpande@icloud.om>
2020-05-05 12:31:25 +0530

    Configured project

---
+++ src/main/java/boogie/utilmod/UsefulUtlitiesClient.java
@@ -0,0 +1,10 @@
+package boogie.utilmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UsefulUtlitiesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Starting Useful Utlities...");
+    }
+}
---
+++ src/main/java/boogie/utilmod/UtilitiesMod.java
@@ -0,0 +1,10 @@
+package boogie.utilmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class UtilitiesMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
